a75a266aa9b10e02351cb7daf984105012b3ee47
Uday1004 <usolanki068@gmail.com>
2023-07-12 11:51:17 +0530

    map

--- src/component/map.js
+++ src/component/map.js
@@ -5,10 +5,8 @@ const map = () => {
     <div>
       <>
         <div className="row w-100 d-flex align-items-center justify-content-center">
-          <div className="col-lg-6 my-4 mx-4 ">
-            <iframe className="w-100 " style={{ height: "400px", allowfullscreen: "", loading: "fast" }}
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d115681.29592731265!2d-77.47713270775661!3d25.0326996781907!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x892f7c99b981dbc9%3A0x2aef01d3485e50d2!2sNassau%2C%20Bahamy!5e0!3m2!1spl!2spl!4v1624445118063!5m2!1spl!2spl"
-            ></iframe>
+          <div className="col-lg-6 my-4 mx-5 map ">
+          <iframe title="My Embedded Content" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16913269.27918603!2d73.75405585225245!3d20.60504185438289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1689095640029!5m2!1sen!2sin" width="385" height="450" style={{border:"0"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
           </div>
         </div>
       </>
