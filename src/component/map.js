969d3dc59103efb0a390fe19771fef9ff3462277
Uday1004 <usolanki068@gmail.com>
2023-07-12 22:27:25 +0530

    first commit

--- src/component/map.js
+++ src/component/map.js
@@ -5,8 +5,9 @@ const map = () => {
     <div>
 
         <div >
-          <div  style={{position:"relative",height:"800px"}}>
-          <iframe title="My Embedded Content" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16913269.27918603!2d73.75405585225245!3d20.60504185438289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1689095640029!5m2!1sen!2sin" width="100%" height="80%" style={{border:"0",position:"absolute"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+          <div  style={{position:"relative",height:"1065px"}}>
+          <iframe title="My Embedded Content" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16913269.27918603!2d73.75405585225245!3d20.60504185438289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1689095640029!5m2!1sen!2sin" 
+          width="100%" height="100%" style={{border:"0",position:"absolute"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
           </div>
         </div>
     </div>
@@ -15,4 +16,3 @@ const map = () => {
 }
 
 export default map
-
