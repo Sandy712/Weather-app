e68148161aa7eea0327b3f9182c943b2f7328fb4
Uday1004 <usolanki068@gmail.com>
2023-07-12 22:21:27 +0530

    first commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,44 +2,111 @@ import React from 'react';
 import {
   MDBFooter,
   MDBContainer,
-  MDBIcon,
+  // MDBIcon,
   MDBInput,
   MDBCol,
-  MDBRow,
+  MDBRow, 
   MDBBtn
 } from 'mdb-react-ui-kit';
-import {RiLinkedinBoxFill} from 'react-icons/ri';
-import {RiTwitterFill} from 'react-icons/ri';
-import {RiGoogleFill} from 'react-icons/ri';
-import {RiInstagramFill} from 'react-icons/ri';
-import {RiGithubFill} from 'react-icons/ri';
+import {RiInstagramFill} from 'react-icons/ri' 
+import {RiTwitterFill} from 'react-icons/ri' 
+import {RiGithubFill} from 'react-icons/ri' 
+import {RiLinkedinBoxFill} from 'react-icons/ri' 
+import {RiStackFill} from 'react-icons/ri' 
+
+ 
+
 
 export default function App() {
   return (
     <MDBFooter className='text-center' color='white' bgColor='dark'>
       <MDBContainer className='p-4'>
         <section className='mb-4'>
-          <MDBBtn outline color="light" floating className='m-1 l-1' href='#!' role='button'>
-            <RiLinkedinBoxFill/>
-          </MDBBtn>
-
-          <MDBBtn outline color="light" floating className='m-1 l-1' href='#!' role='button'>
-            <RiTwitterFill />
-          </MDBBtn>
-
-          <MDBBtn outline color="light" floating className='m-1 l-1' href='#!' role='button'>
-            <RiGoogleFill/>
-          </MDBBtn>
-
-          <MDBBtn outline color="light" floating className='m-1 l-1' href='#!' role='button'>
-            <RiInstagramFill/>
-          </MDBBtn>
-
-          <MDBBtn outline color="light" floating className='m-1 l-1' href='#!' role='button'>
-            <RiGithubFill/>
-          </MDBBtn>
+         
           
         </section>
+        <section className=''>
+        <MDBContainer className='text-center text-md-start mt-5'>
+          <MDBRow className='mt-3'>
+            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                
+              <RiStackFill/> Mausam.ui
+              </h6>
+              <p>
+                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit.
+              </p>
+            </MDBCol>
+
+            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Angular
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  React
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Vue
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Laravel
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Social links</h6>
+              <p>
+                <a href='https://www.instagram.com/udaysolankii03/' className='text-reset'>
+                 <RiInstagramFill/> Instagram
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  <RiTwitterFill/> Twitter
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  <RiGithubFill/> Github
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  <RiLinkedinBoxFill/> Linkdin
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <p>
+                 
+               Inodre , 452011 , India
+              </p>
+              <p>
+                 
+                Mausam.ui.weather@.gmail.com
+              </p>
+              <p>
+                  +91 9174595317
+              </p>
+              <p>
+                  +91 78982 87715
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
 
          
 
--- src/component/Forecaste.js
+++ src/component/Forecaste.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Forecaste() {
   return (
-    <div>
-      <h1 style={{color:'white'}}>work in progess...</h1>
-      <h3 style={{color:'white'}}>Thankyou for Visit</h3>
+    <div style={{marginTop:'20px'}}>
+      <h1 style={{color:'white',marginLeft:'20px'}}>work in progess...</h1>
+      <h3 style={{color:'white', marginLeft:'20px'}}>Thankyou for Visit</h3>
     </div>
   )
 }
