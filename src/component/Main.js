75a5e374c136a0c14b76e41c8133f878cd3781b5
Sandeep Prajapati <sandeepprajapat200@gmail.com>
2023-08-06 17:23:39 +0530

    refactor usereducer

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import { RiSearchLine } from 'react-icons/ri';
 import humi from './images/humidity.png';
 import windi from './images/wind.png';
@@ -10,17 +10,47 @@ import globe from './images/glob.png';
 export default function Main() {
   const apikey = "146df592b88af707529963a10994a183"
 
-  const [city, setCity] = useState("");
-  const [temp, setTemp] = useState("");
-  const [desc, setDesc] = useState("");
-  const [Icon, setIcon] = useState("");
-  const [hum, setHum] = useState("");
-  const [name, setName] = useState("");
-  const [Country, setCountry] = useState("");
-  const [wind, setwind] = useState("");
-  const [isDataFetched, setIsDataFetched] = useState(false);
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_DATA":
+        return {
+          ...state,
+          temp: action.payload.temp,
+          desc: action.payload.desc,
+          hum: action.payload.hum,
+          name: action.payload.name,
+          Icon: action.payload.Icon,
+          Country: action.payload.Country,
+          wind: action.payload.wind,
+          isDataFetched: true,
+        };
+      case "SET_CITY":
+        return {
+          ...state,
+          city: action.payload,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const initialState = {
+    city: "",
+    temp: "",
+    desc: "",
+    Icon: "",
+    hum: "",
+    name: "",
+    Country: "",
+    wind: "",
+    isDataFetched: false,
+  };
 
   const currentDate = new Date().toString().slice(0, 25);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { city, temp, desc, Icon, hum, name, Country, wind, isDataFetched } = state;
+
 
 
   const fetchData = async (e) => {
@@ -30,17 +60,18 @@ export default function Main() {
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
       // Handle the response data
       const onlinedata = response.data;
-      setTemp(onlinedata.main.temp);
-      setDesc(onlinedata.weather[0].description);
-      setHum(onlinedata.main.humidity);
-      setName(onlinedata.name);
-      setIcon(onlinedata.weather[0].icon);
-      setCountry(onlinedata.sys.country);
-      setwind(onlinedata.wind.speed);
-
-      setIsDataFetched(true);
-
-
+      dispatch({
+        type: "SET_DATA",
+        payload: {
+          temp: onlinedata.main.temp,
+          desc: onlinedata.weather[0].description,
+          hum: onlinedata.main.humidity,
+          name: onlinedata.name,
+          Icon: onlinedata.weather[0].icon,
+          Country: onlinedata.sys.country,
+          wind: onlinedata.wind.speed,
+        },
+      });
       console.log(onlinedata)
     } catch (error) {
       // Handle any errors
@@ -56,13 +87,18 @@ export default function Main() {
           `https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=${apikey}&units=metric`
         );
         const onlinedata = response.data;
-        setTemp(onlinedata.main.temp);
-        setDesc(onlinedata.weather[0].description);
-        setHum(onlinedata.main.humidity);
-        setName(onlinedata.name);
-        setIcon(onlinedata.weather[0].icon);
-        setCountry(onlinedata.sys.country);
-        setwind(onlinedata.wind.speed);
+        dispatch({
+          type: "SET_DATA",
+          payload: {
+            temp: onlinedata.main.temp,
+            desc: onlinedata.weather[0].description,
+            hum: onlinedata.main.humidity,
+            name: onlinedata.name,
+            Icon: onlinedata.weather[0].icon,
+            Country: onlinedata.sys.country,
+            wind: onlinedata.wind.speed,
+          },
+        });
 
         // console.log(onlinedata);
       } catch (error) {
@@ -80,21 +116,21 @@ export default function Main() {
 
   return (
     <div className="container d-flex align-items-center justify-content-center" >
-      <div className="container_app" style={{cursor:'auto'}}>
+      <div className="container_app" style={{ cursor: 'auto' }}>
         <div className="Search">
           <form onSubmit={fetchData} className="search-bar" >
             <input
               className="app__input"
               type="text"
               placeholder="Enter City Name"
-              onChange={(e) => setCity(e.target.value)}
+              onChange={(e) => dispatch({ type: "SET_CITY", payload: e.target.value })}
               spellCheck='false'
-              
+
             />
             <button className="button"><RiSearchLine className="search-icon" /></button>
           </form>
         </div>
-           
+
 
 
         <div className="Title">
@@ -104,30 +140,29 @@ export default function Main() {
         <div className="app_desc">
         </div>
 
-      <div className="grid-container">
+        <div className="grid-container">
 
-        <div className="item item1">
-          <img className="app_image" src={`http://openweathermap.org/img/wn/${Icon}.png`} alt="desc" />
-          <h3 style={{cursor:'auto'}}>{desc}</h3>
-        </div>
-        <div className="item item2" >
+          <div className="item item1">
+            <img className="app_image" src={`http://openweathermap.org/img/wn/${Icon}.png`} alt="desc" />
+            <h3 style={{ cursor: 'auto' }}>{desc}</h3>
+          </div>
+          <div className="item item2" >
             <img src={humi} alt="" />
-          <h4 className="app__hum">Humidity: {hum}%</h4>
+            <h4 className="app__hum">Humidity: {hum}%</h4>
+          </div>
+          <div className="item item3">
+            <img src={globe} alt="" />
+            <h4 className="app__count">Country: {Country} </h4>
+          </div>
+          <div className="item item4">
+            <img src={windi} alt="" />
+            <h4 className="app__wind">Wind Speed: {Math.trunc(wind * 3.6)} km/h</h4>
+          </div>
         </div>
-        <div className="item item3">
-          <img src={globe} alt="" />
-          <h4 className="app__count">Country: {Country} </h4>
-        </div>
-        <div className="item item4">
-          <img src={windi} alt="" />
-          <h4 className="app__wind">Wind Speed: {Math.trunc(wind*3.6)} km/h</h4>
-        </div>
-      </div>  
-      <div className="date-time">
+        <div className="date-time">
           <h4 className="app__date">Date: {currentDate}</h4>
-      </div>
+        </div>
       </div>
     </div>
-           
   );
 }
